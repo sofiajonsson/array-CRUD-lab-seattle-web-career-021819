3762c062b0d91b4a7ed33fc01c26b41db3e6c844
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-03 20:19:48 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,29 +7,29 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array.push(array, element)
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_start_of_array.unshift("aa")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  remove_element_from_end_of_array.pop()
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  remove_element_from_start_of_array.shift()
+array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  retrieve_element_from_index()
+ array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
